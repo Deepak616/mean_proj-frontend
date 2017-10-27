77b708f65eb9cb23ebd91b8c33d979af61e844cc
deepak <deepacraj616@gmail.com>
2017-10-27 16:43:31 +0530

    api call for message list in main.js and displaying message list in main.html

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,8 +2,15 @@ export class MainController {
   constructor ($http) {
     'ngInject';
       this.$http=$http;
+      this.getMessages();
     
   }
+    getMessages(){
+        var vm=this
+        this.$http.get('http://localhost:5000/api/getMessage').then(function(res){
+            vm.messages=res.data;
+        })
+    }
     postMessage(){
        this.$http.post('http://localhost:5000/api/postMessage',{msg:this.text});
     }
